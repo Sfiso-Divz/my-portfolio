e7114391df20da35a826b1987a2e049fc43312f4
Mfihlo Dlamini <115468243+Sfiso-Divz@users.noreply.github.com>
2025-03-12 17:32:41 +0200

    my react portfolio updated 4

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,10 @@ const Navbar = () => {
     // Desktop Navbar
     <div className="sticky top-0 bg-white z-50 shadow-xl w-full">
         <div className='hidden md:flex justify-between items-center max-w-[1200px] mx-auto'>
-            <div className="w-24" ><a href="/#home"><img src="./assets/M (1).png" alt="logo" /></a></div>
+            <div className="w-24" ><a href="/#home" onClick={(e) => { 
+    e.preventDefault();
+    document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
+}}><img src="./assets/M (1).png" alt="logo" /></a></div>
             <ul>
                 <li className='flex gap-4 text-sm'>
                     <a href="/#home" onClick={(e) => { 
